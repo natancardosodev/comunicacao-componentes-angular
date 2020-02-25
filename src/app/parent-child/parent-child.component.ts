7ee164b3afd58c9c4b43ef75f12b8c438872fd83
Natan Cardoso <natancb7@gmail.com>
2020-02-25 10:58:25 -0300

    @ViewChild, acessando elementos e metodos

--- src/app/parent-child/parent-child.component.ts
+++ src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild("stopwatch2", {static: false}) private myTimer: TimerComponent;
+  @ViewChild("myP", {static: false}) private myP: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  start() {
+    this.myTimer.start();
+  }
+
+  stop() {
+    this.myTimer.stop();
+  }
+
+  clear() {
+    this.myTimer.clear();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.myP);
   }
 
 }
